222df9a9ce762a3c784df7f986320c09cc8080f2
jingle chen <47862618+jinglechen2287@users.noreply.github.com>
2025-08-30 19:40:45 -0400

    remove button animation to avoid safari issue

--- src/components/ui/primaryButton.tsx
+++ src/components/ui/primaryButton.tsx
@@ -2,91 +2,101 @@ import { css } from "styled-system/css";
 
 export default function PrimaryButton() {
     return (
-        <button
+        <div
             class={css({
-                transform: "scale(0.5)",
-                transformOrigin: "center",
-                transition: "transform 0.1s ease-in-out",
-                backgroundColor: "rgb(255 255 255 / 0.3)",
-                padding: "15px",
-                borderRadius: "33px",
-                border: "none",
                 position: "relative",
-                boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.1)",
-                cursor: "pointer",
-                touchAction: "manipulation",
-                WebkitTapHighlightColor: "transparent",
-                _hover: {
-                    transform: "scale(0.53)",
-                },
-                _active: {
+                display: "inline-block",
+                borderRadius: "33px",
+            })}
+        >
+            <div
+                aria-hidden="true"
+                class={css({
                     transform: "scale(0.5)",
-                },
-                "&:hover:active": {
-                    transform: "scale(0.5)",
-                },
-                _before: {
-                    content: '""',
+                    transformOrigin: "center",
                     display: "block",
                     background:
-                        "conic-gradient(from var(--rotation), purple, blue, green, yellow, orange, red)",
+                        "conic-gradient(purple, blue, green, yellow, orange, red)",
                     filter: "blur(40px)",
                     opacity: 1,
                     position: "absolute",
                     inset: "10px",
                     borderRadius: "3px",
-                    zIndex: 5,
-                    animationName: "rotate",
-                    animationDuration: "7s",
-                    animationTimingFunction: "linear",
-                    animationIterationCount: "infinite",
-                    animationPlayState: "running",
-                },
-                _after: {
-                    content: '""',
-                    display: "block",
-                    background:
-                        "linear-gradient(to bottom,rgb(255 255 255 / 0.5) 0%,rgb(255 255 255 / 0.5) 2px,rgb(100 100 100 / 0.1) 30%,rgb(100 100 100 / 0.1) 70%,rgb(255 255 255 / 0.5) 99%,rgb(255 255 255 / 0.5) 100%)",
-                    position: "absolute",
-                    inset: "-1.5px",
-                    borderRadius: "33px",
-                    zIndex: 6,
-                },
-            })}
-        >
-            <span
+                    zIndex: 0,
+                    pointerEvents: "none",
+                })}
+            />
+            <button
                 class={css({
+                    transform: "scale(0.5)",
+                    transformOrigin: "center",
+                    transition: "transform 0.1s ease-in-out",
+                    backgroundColor: "rgb(255 255 255 / 0.3)",
+                    padding: "15px",
+                    borderRadius: "33px",
+                    border: "none",
                     position: "relative",
-                    display: "inline-block",
-                    background: "linear-gradient(to bottom, #424242, #343434)",
-                    borderRadius: "18px",
-                    fontSize: "42px",
-                    color: "white",
-                    paddingY: "15px",
-                    paddingX: "20px",
-                    borderStyle: "solid",
-                    borderTopColor: "#5b5b5bff",
-                    borderBottomColor: "#282828ff",
-                    borderYWidth: "1px",
-                    boxShadow: "0 8px 10px rgb(0 0 0 / 0.3)",
-                    zIndex: 10,
+                    boxShadow: "0 4px 10px 0 rgba(0, 0, 0, 0.1)",
+                    cursor: "pointer",
+                    touchAction: "manipulation",
+                    WebkitTapHighlightColor: "transparent",
+                    zIndex: 1,
+                    _hover: {
+                        transform: "scale(0.53)",
+                    },
+                    _active: {
+                        transform: "scale(0.5)",
+                    },
+                    "&:hover:active": {
+                        transform: "scale(0.5)",
+                    },
+                    _after: {
+                        content: '""',
+                        display: "block",
+                        background:
+                            "linear-gradient(to bottom,rgb(255 255 255 / 0.5) 0%,rgb(255 255 255 / 0.5) 2px,rgb(100 100 100 / 0.1) 30%,rgb(100 100 100 / 0.1) 70%,rgb(255 255 255 / 0.5) 99%,rgb(255 255 255 / 0.5) 100%)",
+                        position: "absolute",
+                        inset: "-1.5px",
+                        borderRadius: "33px",
+                        zIndex: 6,
+                    },
                 })}
             >
                 <span
                     class={css({
-                        display: "block",
-                        paddingY: "12px",
-                        paddingX: "30px",
-                        borderRadius: "50px",
+                        position: "relative",
+                        display: "inline-block",
                         background:
-                            "linear-gradient(to bottom, #2d2d2dff, #424242)",
+                            "linear-gradient(to bottom, #424242, #343434)",
+                        borderRadius: "18px",
+                        fontSize: "42px",
+                        color: "white",
+                        paddingY: "15px",
+                        paddingX: "20px",
+                        borderStyle: "solid",
+                        borderTopColor: "#5b5b5bff",
+                        borderBottomColor: "#282828ff",
+                        borderYWidth: "1px",
+                        boxShadow: "0 8px 10px rgb(0 0 0 / 0.3)",
                         zIndex: 10,
-                        fontWeight: "semibold",
                     })}
                 >
-                    Get Started
+                    <span
+                        class={css({
+                            display: "block",
+                            paddingY: "12px",
+                            paddingX: "30px",
+                            borderRadius: "50px",
+                            background:
+                                "linear-gradient(to bottom, #2d2d2dff, #424242)",
+                            zIndex: 10,
+                            fontWeight: "semibold",
+                        })}
+                    >
+                        Get Started
+                    </span>
                 </span>
-            </span>
-        </button>
+            </button>
+        </div>
     );
-};
+}
